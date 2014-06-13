2579d3e486153ae1413daf7336b59a624ed488b6
Laurent Sarrazin <laurent@sarrazin.io>
2014-06-13 12:20:01 +0200

    Handle all the parameters

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec /usr/bin/java -jar /evernote-rest-webapp.war --evernote.consumerKey=$CONSUMER_KEY --evernote.consumerSecret=$CONSUMER_SECRET
+exec /usr/bin/java -jar /evernote-rest-webapp.war $*
